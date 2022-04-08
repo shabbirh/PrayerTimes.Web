eea4452959837b041f4c3056ea35c9320b1071ab
Shabbir R Hassanally <shabbir@hassanally.net>
2022-04-08 15:35:56 +0100

    some enhancements and displaying more useful information.  timezone auto detection still needs to be done, same with dst auto detection.

---
+++ PrayerTimes.Web/Pages/Documentation.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using Markdig;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PrayerTimes.Web.Pages
+{
+    public class DocumentationModel : PageModel
+    {
+        private readonly ILogger<DocumentationModel> _logger;
+
+       
+        public string Documentation { get; set; } = string.Empty;
+
+        public DocumentationModel(ILogger<DocumentationModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            string documentation;
+            using (var streamReader = new StreamReader(@"Documentation/Documentation.html", Encoding.UTF8))
+            {
+                documentation = streamReader.ReadToEnd();
+            }
+
+            
+            Documentation = documentation;
+        }
+    }
+}
